32d54aee91a091e59f741add232b02da8ba41978
Jake <jakespittlehouse@outlook.com>
2024-02-01 14:47:06 +0000

    Added miles to km program in main function

--- Comp1721_General_Work/src/Main.java
+++ Comp1721_General_Work/src/Main.java
@@ -1,12 +1,14 @@
-public class Main {
+import java.util.Scanner; // Package
+
+public class Main // If a class is public then the name of the class must match the name of the file
+{
     public static void main(String[] args)
     {
-        System.out.printf("Hello and welcome!");
+        System.out.print("Distance in miles : ");
 
-        for (int i = 1; i <= 5; i++)
-        {
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+        double distance = input.nextDouble();
 
-        }
+        System.out.printf("Distance in km : %.1f km", (distance*1.60934)); // Format string using .printf, similar to c
     }
 }
